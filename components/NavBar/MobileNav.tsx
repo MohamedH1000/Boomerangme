0a68227353add587371cc20e3034b35b7ababfc0
MohamedH1000 <mohammedhisham115@gmail.com>
2024-06-28 22:16:54 +0300

    Boomerangme

--- components/NavBar/MobileNav.tsx
+++ components/NavBar/MobileNav.tsx
@@ -6,17 +6,119 @@ const MobileNav = () => {
   const [open, setIsOpen] = useState(false);
   return (
     <div
-      className="flex justify-center items-center md:hidden max-md:static mr-4"
-      onClick={() => setIsOpen((prev) => !prev)}
+      className="flex justify-center items-center md:hidden max-md:static mr-4 relative"
+      onClick={() => setIsOpen((prev: any) => !prev)}
     >
       {open ? (
-        <Image
-          src={"/assets/close.png"}
-          alt="close"
-          height={20}
-          width={20}
-          className="invert"
-        />
+        <>
+          <Image
+            src={"/assets/close.png"}
+            alt="close"
+            height={20}
+            width={20}
+            className="invert rela"
+          />
+          <div className="w-[320px] absolute right-0  top-[64px] flex flex-col justify-center items-start max-sm:w-full min-h-screen">
+            <div className="w-[320px] h-[114px] bg-[#1F1E1F] border-t-[0.5px] border-white p-4 max-sm:w-full">
+              <div className="flex justify-start items-center gap-10">
+                <div className="text-white text-[12px]">
+                  <h1>
+                    Hi <br />
+                    <span className="font-bold">Mohamed</span>
+                  </h1>
+                </div>
+                <div className="text-white text-[12px]">
+                  <h1>
+                    Plan: <br />
+                    <span className="font-bold">Start</span>
+                  </h1>
+                </div>
+              </div>
+              <div className="mt-4 text-white font-bold">Implementation</div>
+            </div>
+            <div className="bg-[#333] flex flex-1 flex-col  w-full overflow-y-auto z-1">
+              <div className="p-4 h-[68px] border-b-[0.5px] border-[gray] flex justify-between items-center w-full ">
+                <div className="flex justify-center gap-5 items-center">
+                  <Image
+                    src={"/icons/mycards.svg"}
+                    alt="my cards"
+                    width={12}
+                    height={16}
+                    className="invert"
+                  />
+                  <p className="text-white text-[12px]">My Cards:</p>
+                </div>
+                <Image
+                  src={"/icons/downarrow1.svg"}
+                  alt="down arrow"
+                  width={14}
+                  height={16}
+                  className="invert"
+                />
+              </div>
+              <div className="p-4 h-[68px] border-b-[0.5px] border-[gray] flex justify-start items-center gap-5 w-full">
+                <Image
+                  src={"/icons/addcards.svg"}
+                  alt="my cards"
+                  width={16}
+                  height={16}
+                  className="invert"
+                />
+                <p className="text-white text-[12px]">Create card</p>
+              </div>
+              <div className="p-4 h-[68px] border-b-[0.5px] border-[gray] flex justify-start items-center gap-5 w-full">
+                <Image
+                  src={"/icons/customers.svg"}
+                  alt="my cards"
+                  width={16}
+                  height={16}
+                  className="invert"
+                />
+                <p className="text-white text-[12px]">Customers</p>
+              </div>
+              <div className="p-4 h-[68px] border-b-[0.5px] border-[gray] flex justify-start items-center gap-5 w-full">
+                <Image
+                  src={"/icons/mailings.svg"}
+                  alt="my cards"
+                  width={16}
+                  height={16}
+                  className="invert"
+                />
+                <p className="text-white text-[12px]">Push</p>
+              </div>
+              <div className="p-4 h-[68px] border-b-[0.5px] border-[gray] flex justify-start items-center gap-5 w-full">
+                <Image
+                  src={"/icons/locations.svg"}
+                  alt="my cards"
+                  width={16}
+                  height={16}
+                  className="invert"
+                />
+                <p className="text-white text-[12px]">Locations</p>
+              </div>
+              <div className="p-4 h-[68px] border-b-[0.5px] border-[gray] flex justify-start items-center gap-5 w-full">
+                <Image
+                  src={"/assets/statistics.svg"}
+                  alt="my cards"
+                  width={16}
+                  height={16}
+                  className="invert"
+                />
+                <p className="text-white text-[12px]">Account statistics</p>
+              </div>
+              <div className="p-4 h-[68px] border-b-[0.5px] border-[gray] flex justify-start items-center gap-5 w-full">
+                <Image
+                  src={"/icons/manager.svg"}
+                  alt="my cards"
+                  width={16}
+                  height={16}
+                  className="invert"
+                />
+                <p className="text-white text-[12px]">Managers</p>
+              </div>
+            </div>
+          </div>
+        </>
       ) : (
         <Image
           src={"/icons/hamburger.svg"}
